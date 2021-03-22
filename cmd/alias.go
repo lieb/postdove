991f9010084938d71b779a7ba1977947913f9f36
Jim Lieb <lieb@sea-troll.net>
2021-03-22 16:32:45 -0700

    Initial commit of main and CLI infrastructure
    Decided to use github.com/spf13/cobra CLI processing.
    This is the initial top level framework.
    
    Signed-off-by: Jim Lieb <lieb@sea-troll.net>

---
+++ cmd/alias.go
@@ -0,0 +1,102 @@
+/*
+Copyright © 2021 Jim Lieb <lieb@sea-troll.net>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+// importAlias do import of an aliases file
+var importAlias = &cobra.Command{
+	Use:   "alias [ -i file/path]",
+	Short: "Import an alias file in the aliases(5) format",
+	Long: `Import a local aliases file in the aliases(5) format
+from the file named by the -i flag (default stdin '-').
+This is typically the /etc/aliases file that maps various system
+users and email aliases to a specific user or site sysadmin mailbox`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("import alias called")
+	},
+}
+
+// exportAlias do export of an aliases file
+var exportAlias = &cobra.Command{
+	Use:   "alias [ -o file/path]",
+	Short: "Export aliases alias file in the aliases(5) format",
+	Long: `Export a local aliases file in the aliases(5) format to
+the file named by the -o flag (default stdout '-').
+This is typically the /etc/aliases file that maps various system
+users and email aliases to a specific user or site sysadmin mailbox`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("export alias called")
+	},
+}
+
+// addAlias do add of an aliases file
+var addAlias = &cobra.Command{
+	Use:   "alias address recipient ...",
+	Short: "Add an alias into the database",
+	Long: `Add an alias into the database. The address is a local
+user or alias target without a "@domain" part, i.e. "postmaster" or "daemon".
+One or more recipients can be added. A recipient can either be a single local mailbox,
+i.e. "root" or "admin", an RFC2822 format email address, or a file or a pipe to a command.
+ See aliases(5) man page for details.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("add alias called")
+	},
+}
+
+// deleteAlias do delete of an aliases file
+var deleteAlias = &cobra.Command{
+	Use:   "alias address",
+	Short: "Delete an alias from the database.",
+	Long: `Delete an address alias from the database.
+All of the recipients pointed to by this name will be also deleted`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("delete alias called")
+	},
+}
+
+// editAlias do edit of an aliases file
+var editAlias = &cobra.Command{
+	Use:   "alias address",
+	Short: "Edit the alias address and its recipients in the database",
+	Long:  `Edit a local alias address and its list of recipients.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("edit alias called")
+	},
+}
+
+func init() {
+	importCmd.AddCommand(importAlias)
+	exportCmd.AddCommand(exportAlias)
+	addCmd.AddCommand(addAlias)
+	deleteCmd.AddCommand(deleteAlias)
+	editCmd.AddCommand(editAlias)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// aliasCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// aliasCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
---
+++ cmd/root.go
@@ -0,0 +1,88 @@
+/*
+Copyright © 2021 Jim Lieb <lieb@sea-troll.net>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+package cmd
+
+import (
+	"fmt"
+	"github.com/spf13/cobra"
+	"os"
+
+	homedir "github.com/mitchellh/go-homedir"
+	"github.com/spf13/viper"
+)
+
+var (
+	cfgFile string
+	dbFile  string
+)
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "postdove",
+	Short: "A management tool for aliases and mail users of postfix and dovecot",
+	Long: `Postdove is a management tool to manage the sqlite database file that
+is used by postfix to manage aliases, domains, and delivery and by dovecot to 
+manage email user IMAP/POP3 email accounts`,
+	// Uncomment the following line if your bare application
+	// has an action associated with it:
+	Run: func(cmd *cobra.Command, args []string) { fmt.Println("Use the TUI") },
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	cobra.CheckErr(rootCmd.Execute())
+}
+
+func init() {
+	cobra.OnInitialize(initConfig)
+
+	// Here you will define your flags and configuration settings.
+	// Cobra supports persistent flags, which, if defined here,
+	// will be global for your application.
+
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "$HOME/.postdove.yaml", "config file")
+	rootCmd.PersistentFlags().StringVar(&dbFile, "dbfile", "/etc/dovecot/private/dovecot.sqlite",
+		"Sqlite3 database file")
+
+	// Cobra also supports local flags, which will only run
+	// when this action is called directly.
+	// rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
+
+// initConfig reads in config file and ENV variables if set.
+func initConfig() {
+	if cfgFile != "" {
+		// Use config file from the flag.
+		viper.SetConfigFile(cfgFile)
+	} else {
+		// Find home directory.
+		home, err := homedir.Dir()
+		cobra.CheckErr(err)
+
+		// Search config in home directory with name ".postdove" (without extension).
+		viper.AddConfigPath(home)
+		viper.SetConfigName(".postdove")
+	}
+
+	viper.AutomaticEnv() // read in environment variables that match
+
+	// If a config file is found, read it in.
+	if err := viper.ReadInConfig(); err == nil {
+		fmt.Fprintln(os.Stderr, "Using config file:", viper.ConfigFileUsed())
+	}
+}
