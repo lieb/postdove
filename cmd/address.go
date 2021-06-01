9f756f37452b3256f60ebf90ca11ecab97e30df6
Jim Lieb <lieb@sea-troll.net>
2021-06-01 13:58:23 -0700

    Add "address" set of commands
    Needed to edit/add address attributes. Include tests and test
    data file.
    
    Signed-off-by: Jim Lieb <lieb@sea-troll.net>

---
+++ cmd/address.go
@@ -0,0 +1,223 @@
+/*
+Copyright © 2021 Jim Lieb <lieb@sea-troll.net>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+package cmd
+
+import (
+	"fmt"
+	//"strconv"
+	"strings"
+
+	"github.com/lieb/postdove/maildb"
+	"github.com/spf13/cobra"
+)
+
+var (
+	localPart string
+	arClass   string
+)
+
+// importAddress do import of a addresss file
+var importAddress = &cobra.Command{
+	Use:   "address",
+	Short: "Import a file containing an address and its attributes, one per line",
+	Long:  `Import an addresses file from the file named by the -i flag (default stdin '-').`,
+	Args:  cobra.NoArgs,
+	RunE:  addressImport,
+}
+
+// exportAddress do export of a addresses file
+var exportAddress = &cobra.Command{
+	Use:   "address",
+	Short: "Export addresses into file one per line",
+	Long:  `Export addresses to the file named by the -o flag (default stdout '-').`,
+	Args:  cobra.MaximumNArgs(1),
+	RunE:  addressExport,
+}
+
+// addAddress do add of a addresss file
+var addAddress = &cobra.Command{
+	Use:   "address name",
+	Short: "Add an address into the database",
+	Long: `Add an address into the database. The name is either local (just a name with no domain part) or an RFC2822 format address.
+The optional restriction class defines how postfix processes this address.`,
+	Args: cobra.ExactArgs(1),
+	RunE: addressAdd,
+}
+
+// deleteAddress do delete of a addresss file
+var deleteAddress = &cobra.Command{
+	Use:   "address ",
+	Short: "Delete an address from the database.",
+	Long:  `Delete an address address from the database.`,
+	Args:  cobra.ExactArgs(1),
+	RunE:  addressDelete,
+}
+
+// editAddress do edit of a domains file
+var editAddress = &cobra.Command{
+	Use:   "address name",
+	Short: "Edit the named address and attributes in the database",
+	Long:  `Edit a address and its attributes.`,
+	Args:  cobra.ExactArgs(1), // edit just this address
+	RunE:  addressEdit,
+}
+
+// showAddress display address contents
+var showAddress = &cobra.Command{
+	Use:   "address name",
+	Short: "Display the contents of the named address to the standard output",
+	Long: `Show the contents of the named address to the standard output
+showing all its attributes`,
+	Args: cobra.ExactArgs(1),
+	RunE: addressShow,
+}
+
+// linkage to top level commands
+func init() {
+	importCmd.AddCommand(importAddress)
+	exportCmd.AddCommand(exportAddress)
+	addCmd.AddCommand(addAddress)
+	addAddress.Flags().StringVarP(&arClass, "rclass", "r", "",
+		"Restriction class for this address")
+	deleteCmd.AddCommand(deleteAddress)
+	editCmd.AddCommand(editAddress)
+	/*	editAddress.Flags().StringVarP(&dClass, "local-part", "l", "",
+		"Local part (the mailbox name) for this address") */
+	editAddress.Flags().StringVarP(&arClass, "rclass", "r", "",
+		"Restriction class for this address")
+	showCmd.AddCommand(showAddress)
+}
+
+// addressImport the addresss from inFile
+func addressImport(cmd *cobra.Command, args []string) error {
+	var err error
+
+	mdb.Begin()
+	defer mdb.End(&err)
+
+	err = procImport(cmd, POSTFIX, procAddress)
+	return err
+}
+
+// procAddress
+// options are "option=XXX"
+func procAddress(tokens []string) error {
+	var (
+		a   *maildb.Address
+		err error
+	)
+
+	if a, err = mdb.InsertAddress(tokens[0]); err != nil {
+		return err
+	}
+	if len(tokens) > 1 {
+		for _, opt := range tokens[1:] {
+			kv := strings.Split(opt, "=")
+			if len(kv) < 2 {
+				return fmt.Errorf("Badly formed option field key=value pair")
+			}
+			switch kv[0] {
+			case "rclass":
+				if kv[1] == "\"\"" { // fallback to domain rclass
+					err = a.SetRclass(kv[1])
+				}
+			default:
+				return fmt.Errorf("Unknown option %s", kv[0])
+			}
+			if err != nil {
+				break
+			}
+		}
+	}
+	return err
+}
+
+// addressExport the addresss to outFile
+func addressExport(cmd *cobra.Command, args []string) error {
+	var address string
+
+	switch len(args) {
+	case 0: // all addresss
+		address = "*@*"
+	case 1:
+		address = args[0] // addresses by wildcard
+	default:
+		return fmt.Errorf("Only one address can be specified")
+	}
+	al, err := mdb.FindAddress(address)
+	if err == nil {
+		for _, a := range al {
+			cmd.Printf("%s\n", a.Export())
+		}
+	}
+	return err
+}
+
+// addressAdd the address and its class
+func addressAdd(cmd *cobra.Command, args []string) error {
+	var (
+		a   *maildb.Address
+		err error
+	)
+
+	mdb.Begin()
+	defer mdb.End(&err)
+
+	a, err = mdb.InsertAddress(args[0])
+	if err == nil && cmd.Flags().Changed("rclass") {
+		err = a.SetRclass(arClass)
+	}
+	return err
+}
+
+// addressDelete the address in the first arg
+func addressDelete(cmd *cobra.Command, args []string) error {
+	return mdb.DeleteAddress(args[0])
+}
+
+// addressEdit the address in the first arg
+func addressEdit(cmd *cobra.Command, args []string) error {
+	var (
+		err error
+		a   *maildb.Address
+	)
+
+	mdb.Begin()
+	defer mdb.End(&err)
+
+	a, err = mdb.GetAddress(args[0])
+	if err == nil && cmd.Flags().Changed("rclass") {
+		err = a.SetRclass(arClass)
+	}
+	return err
+}
+
+// addressShow
+func addressShow(cmd *cobra.Command, args []string) error {
+	var (
+		err error
+		a   *maildb.Address
+	)
+
+	if a, err = mdb.LookupAddress(args[0]); err != nil {
+		return err
+	}
+	cmd.Printf("Address:\t\t%s\nTransport:\t%s\nAccess:\t\t%s\n",
+		a.String(), a.Transport(), a.Access())
+	cmd.Printf("Restrictions:\t%s\n", a.Rclass())
+	return nil
+}
---
+++ cmd/address_test.go
@@ -0,0 +1,290 @@
+/*
+Copyright © 2021 Jim Lieb <lieb@sea-troll.net>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+package cmd
+
+import (
+	//"bytes"
+	"fmt"
+	"github.com/lieb/postdove/maildb"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"strings"
+	"testing"
+	// "github.com/spf13/cobra"
+)
+
+// Test_Domain
+func Test_Address(t *testing.T) {
+	var (
+		err         error
+		dir         string
+		dbfile      string
+		args        []string
+		out, errout string
+	)
+
+	fmt.Println("Test_Address")
+
+	// Make a database and test it
+	dir, err = ioutil.TempDir("", "TestAddress-*")
+	defer os.RemoveAll(dir)
+	dbfile = filepath.Join(dir, "test.db")
+
+	// Now create a good database
+	args = []string{"create", "-d", dbfile, "-s", "../schema.sql"}
+	out, errout, err = doTest(rootCmd, "", args)
+	if err != nil {
+		t.Errorf("Create DB: Unexpected error, %s", err)
+	}
+	if out != "" {
+		t.Errorf("Create DB: did not expect output, got %s", out)
+	}
+	if errout != "" {
+		t.Errorf("Create DB: did not expect error output, got %s", errout)
+	}
+
+	// Add some addresses, first with just defaults
+	args = []string{"-d", dbfile, "add", "address", "bill@somewhere.org"} // using default class
+	out, errout, err = doTest(rootCmd, "", args)
+	if err != nil {
+		t.Errorf("Add bill@somewhere.org: Unexpected error, %s", err)
+	}
+	if out != "" {
+		t.Errorf("Add bill@somewhere.org: did not expect output, got %s", out)
+	}
+	if errout != "" {
+		t.Errorf("Add bill@somewhere.org: did not expect error output, got %s", errout)
+	}
+	args = []string{"-d", dbfile, "show", "address", "bill@somewhere.org"} // now look it up
+	out, errout, err = doTest(rootCmd, "", args)
+	if err != nil {
+		t.Errorf("Show of bill@somewhere.org in good DB: Unexpected error, %s", err)
+	}
+	if out != "Address:\t\tbill@somewhere.org\nTransport:\t--\nAccess:\t\t--\nRestrictions:\t--\n" {
+		t.Errorf("Show of bill@somewhere.org in good DB: did not get expected output, got %s", out)
+	}
+	if errout != "" {
+		t.Errorf("show of bill@somewhere.org in good DB: did not expect error output, got %s", errout)
+	}
+	// and a local address
+	args = []string{"-d", dbfile, "add", "address", "postmaster"} // using default class
+	out, errout, err = doTest(rootCmd, "", args)
+	if err != nil {
+		t.Errorf("Add postmaster: Unexpected error, %s", err)
+	}
+	if out != "" {
+		t.Errorf("Add postmaster: did not expect output, got %s", out)
+	}
+	if errout != "" {
+		t.Errorf("Add postmaster: did not expect error output, got %s", errout)
+	}
+	args = []string{"-d", dbfile, "show", "address", "postmaster"} // now look it up
+	out, errout, err = doTest(rootCmd, "", args)
+	if err != nil {
+		t.Errorf("Show of postmaster in good DB: Unexpected error, %s", err)
+	}
+	if out != "Address:\t\tpostmaster\nTransport:\t--\nAccess:\t\t--\nRestrictions:\t--\n" {
+		t.Errorf("Show of postmaster in good DB: did not get expected output, got %s", out)
+	}
+	if errout != "" {
+		t.Errorf("show of postmaster in good DB: did not expect error output, got %s", errout)
+	}
+
+	// now with a valid option
+	args = []string{"-d", dbfile, "add", "address", "dave@somewhere.org", "--rclass", "DUMP"}
+	out, errout, err = doTest(rootCmd, "", args)
+	if err != nil {
+		t.Errorf("Add dave@somewhere.org: Unexpected error, %s", err)
+	}
+	if out != "" {
+		t.Errorf("Add dave@somewhere.org: did not expect output, got %s", out)
+	}
+	if errout != "" {
+		t.Errorf("Add dave@somewhere.org: did not expect error output, got %s", errout)
+	}
+	args = []string{"-d", dbfile, "show", "address", "dave@somewhere.org"} // now look it up
+	out, errout, err = doTest(rootCmd, "", args)
+	if err != nil {
+		t.Errorf("Show of dave@somewhere.org in good DB: Unexpected error, %s", err)
+	}
+	if out != "Address:\t\tdave@somewhere.org\nTransport:\t--\nAccess:\t\t--\nRestrictions:\tDUMP\n" {
+		t.Errorf("Show of dave@somewhere.org in good DB: did not get expected output, got %s", out)
+	}
+	if errout != "" {
+		t.Errorf("show of dave@somewhere.org in good DB: did not expect error output, got %s", errout)
+	}
+
+	// now with a bogus option
+	args = []string{"-d", dbfile, "add", "address", "steve@somewhere.org", "-B", "88"}
+	out, errout, err = doTest(rootCmd, "", args)
+	if err == nil {
+		t.Errorf("Bad add of steve@somewhere.org: Should have failed")
+	} else if !strings.Contains(err.Error(), "unknown shorthand flag: 'B'") {
+		t.Errorf("Bad add of steve@somewhere.org: Unexpected error %s", err)
+	}
+	if out == "" {
+		t.Errorf("Bad add steve@somewhere.org: Expected output, got nothing")
+	}
+	if errout == "" {
+		t.Errorf("Bdd add steve@somewhere.org: Expected error output, got nothing")
+	}
+
+	// Now edit bill@somewhere.org
+	args = []string{"-d", dbfile, "edit", "address", "bill@somewhere.org", "--rclass", "STALL"}
+	out, errout, err = doTest(rootCmd, "", args)
+	if err != nil {
+		t.Errorf("Edit bill@somewhere.org: Unexpected error, %s", err)
+	}
+	if out != "" {
+		t.Errorf("Edit bill@somewhere.org: did not expect output, got %s", out)
+	}
+	if errout != "" {
+		t.Errorf("Edit bill@somewhere.org: did not expect error output, got %s", errout)
+	}
+	args = []string{"-d", dbfile, "show", "address", "bill@somewhere.org"} // now look it up
+	out, errout, err = doTest(rootCmd, "", args)
+	if err != nil {
+		t.Errorf("Show of bill@somewhere.org in good DB: Unexpected error, %s", err)
+	}
+	if out != "Address:\t\tbill@somewhere.org\nTransport:\t--\nAccess:\t\t--\nRestrictions:\tSTALL\n" {
+		t.Errorf("Show of bill@somewhere.org in good DB: did not get expected output, got %s", out)
+	}
+	if errout != "" {
+		t.Errorf("Show of bill@somewhere.org in good DB: did not expect error output, got %s", errout)
+	}
+
+	// delete a domain and check, starting with a non-existent
+	args = []string{"-d", dbfile, "delete", "address", "steve@somewhere.org"}
+	out, errout, err = doTest(rootCmd, "", args)
+	if err == nil {
+		t.Errorf("Delete steve@somewhere.org: should have failed")
+	} else if err != maildb.ErrMdbAddressNotFound {
+		t.Errorf("Delete steve@somwhere.org: Unexpected error, %s", err)
+	}
+	if out == "" {
+		t.Errorf("Delete steve@somewhere.org: Expected formatted help output, got nothing")
+	}
+	if !strings.Contains(errout, "address not found") {
+		t.Errorf("Delete steve@somewhere.org: Expected error output, got %s", errout)
+	}
+
+	args = []string{"-d", dbfile, "delete", "address", "dave@somewhere.org"}
+	out, errout, err = doTest(rootCmd, "", args)
+	if err != nil {
+		t.Errorf("Delete dave@somewhere.org: Unexpected error, %s", err)
+	}
+	if out != "" {
+		t.Errorf("Delete dave@somewhere.org: Expected no output, got %s", out)
+	}
+	if errout != "" {
+		t.Errorf("Delete dave@somewhere.org: Expected no error output, got %s", errout)
+	}
+
+	// Now see if it is still there
+	args = []string{"-d", dbfile, "show", "address", "dave@somewhere.org"} // now look it up
+	out, errout, err = doTest(rootCmd, "", args)
+	if err == nil {
+		t.Errorf("Show dave@somewhere.org: should have failed")
+	} else if err != maildb.ErrMdbAddressNotFound {
+		t.Errorf("Show of dave@somewhere.org in good DB: Unexpected error, %s", err)
+	}
+	if out == "" {
+		t.Errorf("Show of dave@somewhere.org: Expected formatted help output, got nothing")
+	}
+	if !strings.Contains(errout, "address not found") {
+		t.Errorf("Show of dave@somewhere.org: Expected error output, got %s", errout)
+	}
+
+	// import some addresses first from stdin
+	args = []string{"-d", dbfile, "import", "address"}
+	inputStr := `
+# Just one to test stdin
+mike@bill.org rclass=HOME
+`
+	out, errout, err = doTest(rootCmd, inputStr, args)
+	if err != nil {
+		t.Errorf("Import of mike@bill.org: Unexpected error, %s", err)
+	}
+	if out != "" {
+		t.Errorf("Import of mike@bill.org: Expected no output, got %s", out)
+	}
+	if errout != "" {
+		t.Errorf("Import of mike@bill.org: Expected no error output, got %s", errout)
+	}
+	// and now a file
+	args = []string{"-d", dbfile, "import", "address", "-i", "./test_addresses.txt"}
+	out, errout, err = doTest(rootCmd, "", args)
+	if err != nil {
+		t.Errorf("Import of addresses from file: Unexpected error, %s", err)
+	}
+	if out != "" {
+		t.Errorf("Import of addresses from file: Expected no output, got %s", out)
+	}
+	if errout != "" {
+		t.Errorf("Import of addresses from file: Expected no error output, got %s", errout)
+	}
+
+	// export list to date...
+	exportList := "mike@bill.org rclass=\"\"\n" +
+		"gramma@cottage rclass=\"\"\n" +
+		"wolf@forest rclass=\"\"\n" +
+		"mary@little.lamb rclass=\"\"\n" +
+		"bill@somewhere.org rclass=STALL\n"
+	// now check the contents.
+	args = []string{"-d", dbfile, "export", "address"}
+	out, errout, err = doTest(rootCmd, "", args)
+	if err != nil {
+		t.Errorf("Export addresses: Unexpected error, %s", err)
+	}
+	if out != exportList {
+		t.Errorf("Export addresses: Expected export list[%d](%s), got [%d](%s)",
+			len(exportList), exportList, len(out), out)
+	}
+	if errout != "" {
+		t.Errorf("Export addresses: Expected no error output, got %s", errout)
+	}
+	// now check using wildcard "*" for local addresses
+	exportList = "postmaster rclass=\"\"\n"
+	args = []string{"-d", dbfile, "export", "address", "*"}
+	out, errout, err = doTest(rootCmd, "", args)
+	if err != nil {
+		t.Errorf("Export * addresses: Unexpected error, %s", err)
+	}
+	if out != exportList {
+		t.Errorf("Export * addresses: Expected export list[%d](%s), got [%d](%s)",
+			len(exportList), exportList, len(out), out)
+	}
+	if errout != "" {
+		t.Errorf("Export * addresses: Expected no error output, got %s", errout)
+	}
+	// now check just *@*.org
+	exportList = "mike@bill.org rclass=\"\"\n" +
+		"bill@somewhere.org rclass=STALL\n"
+	args = []string{"-d", dbfile, "export", "address", "*@*.org"}
+	out, errout, err = doTest(rootCmd, "", args)
+	if err != nil {
+		t.Errorf("Export *@*.org addresses: Unexpected error, %s", err)
+	}
+	if out != exportList {
+		t.Errorf("Export *@*.org addresses: Expected export list[%d](%s), got [%d](%s)",
+			len(exportList), exportList, len(out), out)
+	}
+	if errout != "" {
+		t.Errorf("Export *@*.org addresses: Expected no error output, got %s", errout)
+	}
+}
