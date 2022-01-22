2a2401cd7102a5413836d13d215ec6dfa6fa1a77
Jim Lieb <lieb@sea-troll.net>
2022-01-21 17:24:45 -0800

    Update commands test_all.sh
    * Add new tests and move the import test to first since they
      are not database related
    
    * Add expanded create tests. These are at the end because all
      the other tests are with a bare database.
    
    Signed-off-by: Jim Lieb <lieb@sea-troll.net>

--- cmd/test_all.sh
+++ cmd/test_all.sh
@@ -1,8 +1,10 @@
 #! /usr/bin/bash
 
-go test -run=Test_Cmds
 go test -run=Test_Import
+go test -run=Test_Cmds
 go test -run=Test_Domain
 go test -run=Test_Address
 go test -run=TestAliasCmds
 go test -run=TestVMailboxCmd
+go test -run=Test_Create
+go test -run=TestCreateNoAliases
