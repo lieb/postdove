c2871dc882f2ded46b00eaf974de4c62d132a7c4
Jim Lieb <lieb@sea-troll.net>
2022-02-20 16:17:10 -0800

    Add transport and access command tests to list
    Signed-off-by: Jim Lieb <lieb@sea-troll.net>

--- cmd/test_all.sh
+++ cmd/test_all.sh
@@ -2,6 +2,11 @@
 
 go test -run=Test_Import
 go test -run=Test_Cmds
+go test -run=TestAccess
+go test -run=TestTransport_
+go test -run=TestTransportEdit
+go test -run=TestTransportAdd_
+go test -run=TestTransportAddOne
 go test -run=Test_Domain
 go test -run=Test_Address
 go test -run=TestAliasCmds
